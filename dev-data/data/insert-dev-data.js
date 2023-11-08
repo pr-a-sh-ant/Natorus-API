dec1c5e8227c37165ecf2d9ee7b691bb11dd69f7
pr-a-sh-ant <prashantaryal07@gmail.com>
2023-11-08 22:59:34 +0545

    adding data to database from the file

---
+++ dev-data/data/insert-dev-data.js
@@ -0,0 +1,47 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
+
+//Read JSON FILE
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+//IMPOR DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}

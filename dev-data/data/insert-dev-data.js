7fd39a483706f7effee47a55e2f118308d56a9d9
pr-a-sh-ant <prashantaryal07@gmail.com>
2023-11-26 23:31:46 +0545

    dont nothing just commited for fun

--- dev-data/data/insert-dev-data.js
+++ dev-data/data/insert-dev-data.js
@@ -14,9 +14,7 @@ mongoose.connect(DB).then(() => console.log('DB connection successful'));
 
 //Read JSON FILE
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 //IMPOR DATA INTO DB
 const importData = async () => {
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
+const User = require('./usersModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -81,6 +82,31 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      //GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -95,6 +121,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 //DOCUMNET MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
